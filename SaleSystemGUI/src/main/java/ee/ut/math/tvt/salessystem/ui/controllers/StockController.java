011aa16dbed027b0eb1e954796afc2a2ed87eb81
martink6nnu <85567497+martink6nnu@users.noreply.github.com>
2023-10-24 21:34:47 +0300

    close #PE-29 #

--- SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/StockController.java
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/StockController.java
@@ -4,10 +4,12 @@ import com.sun.javafx.collections.ObservableListWrapper;
 import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
 import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,12 +17,27 @@ import java.util.ResourceBundle;
 public class StockController implements Initializable {
 
     private final SalesSystemDAO dao;
-
+    String itemName;
+    String itemDescription;
+    int itemQuantity;
+    double itemPrice;
     @FXML
     private Button addItem;
     @FXML
     private TableView<StockItem> warehouseTableView;
+    @FXML
+    private TextField itemNameField;
+    @FXML
+    private TextField itemQuantityField;
+    @FXML
+    private TextField itemDescriptionField;
 
+    @FXML
+    private TextField itemPriceField;
+    @FXML
+    public void onAddItemClicked(){
+        addItem();
+    };
     public StockController(SalesSystemDAO dao) {
         this.dao = dao;
     }
@@ -29,6 +46,16 @@ public class StockController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         refreshStockItems();
         // TODO refresh view after adding new items
+    }
+    public void addItem(){
+        long barCode = dao.findStockItems().size();
+        itemName = itemNameField.getText();
+        itemDescription = itemDescriptionField.getText();
+        itemPrice = Double.parseDouble(itemPriceField.getText());
+        itemQuantity = Integer.parseInt(itemQuantityField.getText());
+        StockItem newStockItem = new StockItem(barCode, itemName, itemDescription, itemPrice, itemQuantity);
+        dao.saveStockItem(newStockItem);
+        refreshStockItems();
     }
 
     @FXML
